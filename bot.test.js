ca0d0ffc2f0b54c9c39996f4964620cf33d0fb48
Andreas <andreasgdp@gmail.com>
2019-08-02 21:39:59 +0200

    Testing with process.env

--- bot.test.js
+++ bot.test.js
@@ -3,14 +3,10 @@ const {
   checkingTesting,
 
   // Variables
-  testingSettings,
+  testing,
 } = require('./bot')
 
 
 test('checking to see if testing-mode is on', async () => {
-  const onlineSettings = require('./ignored_folder/ignoredsettings.json')
-  expect(checkingTesting(testingSettings, 'BOT_TOKEN')).toBe(onlineSettings['BOT_TOKEN']);
-
-  // const testSettings = require('./ignored_folder/ignoredsettings.json')
-  // expect(checkingTesting(testingSettings, 'token')).toBe(testSettings['token']);
+  expect(checkingTesting(testing)).toBe('online');
 });
