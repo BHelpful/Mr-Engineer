fdae508aa24c5660322f5798d13cb55b4e2663b5
Andreas <andreasgdp@gmail.com>
2019-08-02 13:26:24 +0200

    new server test

--- bot.test.js
+++ bot.test.js
@@ -8,8 +8,6 @@ const {
 
 
 test('checking to see if testing-mode is on', async () => {
-  expect(checkingTesting(testingSettings, 'BOT_TOKEN')).toBe(process.env['BOT_TOKEN']);
-
-  // const testSettings = require('./ignored_folder/ignoredsettings.json')
-  // expect(checkingTesting(testingSettings, 'token')).toBe(testSettings['token']);
+  const onlineSettings = require('./ignored_folder/ignoredsettings.json')
+  expect(checkingTesting(testingSettings, 'BOT_TOKEN')).toBe(onlineSettings['BOT_TOKEN']);
 });
