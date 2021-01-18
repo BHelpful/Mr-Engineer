2132be08951e2e9d99b8de2828c201f412e7b47f
Andreas <andreasgdp@gmail.com>
2021-01-18 14:58:28 +0100

    Fixed some issues w. missing packages

--- Test/bot.test.js
+++ Test/bot.test.js
@@ -4,7 +4,7 @@ const {
 
 	// Variables
 	testing,
-} = require('./bot');
+} = require('../App/bot.js');
 
 test('checking to see if testing-mode is on', async () => {
 	expect(checkingTesting(testing)).toBe('online');
