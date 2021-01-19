8273839ff4e084757ba9885ad0c6ef861e57643b
Andreas <andreasgdp@gmail.com>
2021-01-19 12:37:51 +0100

    Fixed 8ball

--- App/Commands/8ball.js
+++ App/Commands/8ball.js
@@ -1,18 +1,25 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-  if (!args[2]) return message.reply('Please ask a full question!')
-  let replies = ['Yes.', 'No.', 'maybe', "I don't know.", 'Ask again.', 'How should I know?', 'Ask again later.']
-  let result = Math.floor((Math.random() * replies.length))
-  let question = args.slice(0).join(' ')
-  let ballEmbed = new Discord.RichEmbed()
-    .setColor('#FF9900')
-    .addField('Question', question)
-    .addField('Answer', replies[result])
-  message.channel.send(ballEmbed)
-}
+	if (!args[2]) return message.reply('Please ask a full question!');
+	let replies = [
+		'Yes.',
+		'No.',
+		'maybe',
+		"I don't know.",
+		'Ask again.',
+		'How should I know?',
+		'Ask again later.',
+	];
+	let result = Math.floor(Math.random() * replies.length);
+	let question = args.slice(0).join(' ');
+	let ballEmbed = new Discord.MessageEmbed()
+		.setColor('#FF9900')
+		.addField('Question', question)
+		.addField('Answer', replies[result]);
+	message.channel.send(ballEmbed);
+};
 
 module.exports.help = {
-  name: '8ball'
-
-}
+	name: '8ball',
+};
